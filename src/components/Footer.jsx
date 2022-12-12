e7602090599976b16046759db399276f5e14a407
aswnss <aswnss.m@gmail.com>
2022-12-13 01:26:02 +0530

    logos added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,12 @@ import "../styles/Footer.css"
 function Footer() {
   return (
     <div className='Footer'>
-        <div className="socialmedia">
-
+        <div className="socialmedia" id='socialmedia'>
+        <h4>Follow Us : </h4>
+        <a href="https://www.linkedin.com/company/gdsc-sctce/"><i class="fa-brands fa-xl fa-linkedin"></i></a>
+        <a href="https://github.com/gdsc-sctce"><i class="fa-brands fa-xl fa-github"></i></a>
+        <a href="https://instagram.com/gdscsctce"><i class="fa-brands fa-xl fa-instagram"></i></a>
+        <a href="https://discord.gg/HgKeTZK5yN"><i class="fa-brands fa-xl fa-discord"></i></a>
         </div>
         <h6 className='credits'>Made by <a href="www.linkedin.com/in/aswnss">@aswnss</a> </h6>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import rebootedlogo from "../assets/Rebooted Logo.png"
+import gdsclogo from "../assets/gdsc-black.png"
 import "../styles/Navbar.css"
 function Navbar() {
   return (
     <div className='navbar'>
-        <a href="">rebootEd</a>
+        <a href=""><img src={rebootedlogo} alt="rebootEd" /></a>
+        <a href=""><img src={gdsclogo} alt="GDSC SCTCE" id='gdsc-logo' /></a>
         <a href="">Home</a>
     </div>
   )
