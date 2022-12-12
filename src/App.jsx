b6c1eb7e0971c1875d2f952638a740668d7cb994
aswnss <aswnss.m@gmail.com>
2022-12-12 20:48:16 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar'
+import Questions from './components/Questions'
+import './styles/App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Questions/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/Navbar.css"
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <a href="">rebootEd</a>
+        <a href="">Home</a>
+    </div>
+  )
+}
+
+export default Navbar
