3cfb779714c4ca348e08d8983e96916f217941e6
aswnss <aswnss.m@gmail.com>
2022-12-13 09:03:33 +0530

    fixed navbar and footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import rebootedlogo from "../assets/Rebooted Logo.png"
-import gdsclogo from "../assets/gdsc-black.png"
+// import gdsclogo from "../assets/gdsc-black.png"
 import "../styles/Navbar.css"
 function Navbar() {
   return (
     <div className='navbar'>
-        <a href=""><img src={rebootedlogo} alt="rebootEd" /></a>
-        <a href=""><img src={gdsclogo} alt="GDSC SCTCE" id='gdsc-logo' /></a>
-        <a href="">Home</a>
+        <a href="https://rebooted.web.app/"><img src={rebootedlogo} alt="rebootEd" id='rebooted' /></a>
+        <a href="https://gdsc.community.dev/sree-chitra-thirunal-college-of-engineering-thiruvananthapuram/"><img src="" alt="GDSC SCTCE" id='gdsc-logo' /></a>
+        <a href="#">Home</a>
     </div>
   )
 }
